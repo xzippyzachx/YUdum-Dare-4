dffffeee503af6e4995d94d4c335183bd7b3d843
Zach <zacharyross1234@gmail.com>
2023-09-02 13:49:01 -0400

    Splash screen & Level select scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -33,6 +35,14 @@ public class GameManager : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             GameUI.Singleton.ShowCursor(!Cursor.visible);
+        }
+
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            GameUI.Singleton.TransitionOut();
+            DOTween.Sequence().AppendInterval(0.5f).OnComplete(() => {
+                SceneManager.LoadScene("LevelSelect");
+            });
         }
     }
 }
